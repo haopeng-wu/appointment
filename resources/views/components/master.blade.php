ad4e9eb5cc5fbe93d5ead1423f8b62ccedb63d0d
HopeWu <767088240@qq.com>
2021-05-28 10:00:17 +0800

    add tailwind

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
     <title>Were</title>
+    <meta charset="UTF-8">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
 {{$slot}}
