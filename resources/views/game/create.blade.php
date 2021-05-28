33dc852a6639a606454d730e562dc3adfcfcb703
HopeWu <767088240@qq.com>
2021-05-28 17:20:54 +0800

    home page tweaks

--- resources/views/game/create.blade.php
+++ resources/views/game/create.blade.php
@@ -1,21 +1,21 @@
 <x-master>
-<h2>Let's create a game:</h2>
+<h2 class="uppercase text-2xl mb-4">Let's create a game:</h2>
 <form class="text-xl" method="post" action="/game/store">
-    <div class="mb-2">
+    <div class="mb-4">
         <label for="total-player">玩家数:</label>
         <input class="border border-gray-400 w-8" type="string" name="total-player">
         @error('total-player')
         <p class='text-red-500 text-xs mt-2'>{{$message}}</p>
         @enderror
     </div>
-    <div class="mb-2">
+    <div class="mb-4">
         <label for="total-player">村名数:</label>
         <input class="border border-gray-400 w-8" type="string" name="total-player">
         @error('total-player')
         <p class='text-red-500 text-xs mt-2'>{{$message}}</p>
         @enderror
     </div>
-    <div class="mb-2">
+    <div class="mb-4">
         女巫<input class="mr-2" type="checkbox" name="special" value="witch">
         预言家<input class="mr-2" type="checkbox" name="special" value="prophet">
         猎人<input class="mr-2" type="checkbox" name="special" value="hunter">
@@ -24,7 +24,7 @@
         狼王<input class="mr-2" type="checkbox" name="special" value="wolf-king">
         白狼王<input class="mr-2" type="checkbox" name="special" value="white-wolf-king">
     </div>
-    <div class="mb-2">
+    <div class="mb-4">
         <label for="total-player">狼人数:</label>
         <input class="border border-gray-400 w-8" type="string" name="total-player">
         @error('total-player')
