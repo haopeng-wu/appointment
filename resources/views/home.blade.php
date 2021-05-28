0a15857b1f5b08add95cbb63bcde1910b1dbe7ee
HopeWu <767088240@qq.com>
2021-05-28 11:20:03 +0800

    home page tweaks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,15 @@
 <x-master>
-    <div><h1>Home</h1></div>
+    <h1>Home</h1>
     <div class="text-xl">
-        <text>Start a Game</text>
+        <a href="/game/create">建房</a>
     </div>
+
+    <form method="post" action="/game/enter">
+        @csrf
+        <label for="roomId">请输入房间编号</label>
+        <input type="number" name="roomId">
+        <button type='submit' class='bg-blue-500 rounded-full shadow py-2 px-4 text-white'>
+            进入房间
+        </button >
+    </form>
 </x-master>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     #return view('welcome');
     return view('home');
 });
+Route::get('/startGame', function () {
+    #return view('welcome');
+    return view('home');
+});
 
 
 Auth::routes();
