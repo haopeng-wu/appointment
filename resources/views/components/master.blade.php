258c324aaec0357b85159daec7a8577fe570e32c
HopeWu <767088240@qq.com>
2021-05-28 11:59:05 +0800

    home page tweaks

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
-<div class="h-2 bg-blue-300">
+<div class="bg-blue-300">
     <h2>Werewolf</h2>
 </div>
 {{$slot}}
