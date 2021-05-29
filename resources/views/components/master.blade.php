93a60ecb5787ea13661942f68d88a4f390cc1d05
HopeWu <767088240@qq.com>
2021-05-29 18:37:17 +0800

    store a game v0.1

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -7,9 +7,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
-<div class="bg-blue-300">
-    <h2>Werewolf</h2>
-    <a href="/"><h2>Home</h2></a>
+<div class="bg-blue-300 flex">
+    <h2 class="flex-1">Werewolf</h2>
+    <a class="flex-1" href="/"><h2>Home</h2></a>
 </div>
 {{$slot}}
 </body>
