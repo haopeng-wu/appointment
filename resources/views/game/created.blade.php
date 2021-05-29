937a13c0082bbd44d2e59ce7e770a2fcc0852d32
HopeWu <767088240@qq.com>
2021-05-29 18:22:39 +0800

    store a game v0.1

--- resources/views/game/created.blade.php
+++ resources/views/game/created.blade.php
@@ -1,3 +1,3 @@
 <x-master>
-    <h1>房间创建成功！房间号为: <span class="font-bold text-red rounded">{{$gameId}}</span>。</h1>
+    <h1>房间创建成功！房间号为: <span class="font-bold text-red-700 text-xl rounded">{{$gameId}}</span>。</h1>
 </x-master>
