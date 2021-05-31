b6526557a367880b7f58078c3ec8d89c537f3d27
HopeWu <767088240@qq.com>
2021-05-31 17:51:15 +0800

    Beautify the home page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,18 @@
 <x-master>
     <div class="container max-w-md mx-auto  justify-center">
-        <div class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xl">
-            <a href="/game/create">建房</a>
-        </div>
+        <div class>
+            <div class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xl">
+                <a href="/game/create">建房</a>
+            </div>
 
-        <form method="post" action="/game/enter">
-            @csrf
-            <label for="roomId">请输入房间编号：</label>
-            <input type="string" name="roomId" class="border border-blue-500 shadow">
-            <button type='submit' class='bg-blue-500 rounded-full shadow py-2 px-4 text-white'>
-                进入房间
-            </button >
-        </form>
+            <form method="post" action="/game/enter">
+                @csrf
+                <label for="roomId">请输入房间编号：</label>
+                <input type="string" name="roomId" class="border border-blue-500 shadow">
+                <button type='submit' class='bg-blue-500 rounded-full shadow py-2 px-4 text-white'>
+                    进入房间
+                </button >
+            </form>
+        </div>
     </div>
 </x-master>
