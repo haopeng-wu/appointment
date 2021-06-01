218b15225fca43b963ad5c08f7c39e712be242b0
HopeWu <767088240@qq.com>
2021-06-01 18:25:11 +0800

    Beautify the home page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,8 @@
                   method="post" action="/game/enter">
                 @csrf
                 <input class="w-full bg-transparent focus-within:outline-none px-4"
-                       type="string" name="roomId" placeholder="请输入房间号">
+                       type="string" name="roomId" placeholder="请输入房间号"
+                       required>
                 <button class='bg-blue-500 rounded-full py-2 px-4 text-white
                 hover:shadow hover:bg-blue-600'
                         style="min-width:100px;"
