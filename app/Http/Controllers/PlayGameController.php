1fcdb5d0ddf93313a55a8a3ef3ead3af897c15ae
HopeWu <767088240@qq.com>
2021-06-01 23:37:06 +0800

    shuffle cards test

--- app/Http/Controllers/PlayGameController.php
+++ app/Http/Controllers/PlayGameController.php
@@ -5,10 +5,36 @@ namespace App\Http\Controllers;
 use App\Models\Game;
 use App\Models\User;
 use App\Models\PlayGame;
+use App\Models\Card;
 use Illuminate\Http\Request;
 
 class PlayGameController extends Controller
 {
+    /**
+     * shuffle cards for a game and distribute them.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function shuffle(Request $request)
+    {
+        # get the room id
+        $roomId=$request['_roomId'];
+        # get all the cards
+        $card_ids=Card::all()->pluck('id');
+        # shuffle the cards
+        shuffle($card_ids);
+        # pick the first one for the user
+        $card_id=$card_ids[0];
+        $card_name=Card::find($card_id);
+
+        $user_id=$request->cookie('user_id');
+        $user=User::find($user_id);
+        PlayGame::create(['game_id'=>$roomId, 'player_id'=>$user_id, 'card_id'=>$card_id]);
+        return "你的身份牌是".$card_name;
+
+    }
+
+
     /**
      * Enter a game.
      *
--- resources/views/game/room.blade.php
+++ resources/views/game/room.blade.php
@@ -3,7 +3,8 @@
         <h1>Room<span class="text-red-600 font-bold ml-1">{{$roomId}}</span></h1>
         <div>
             <form method="post"
-                  action="/game/shuffle">
+                  action="/game/shuffle-cards">
+                <input type="hidden" name="_roomId" value="{{$roomId}}">
                 <button class='bg-blue-500 rounded-full py-2 px-4 text-white
                 hover:shadow hover:bg-blue-600 focus-within:outline-none'
                         style="min-width:100px;"
