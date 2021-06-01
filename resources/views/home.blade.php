318acfc05a812f09105b7eb4a5e341102e223329
HopeWu <767088240@qq.com>
2021-06-01 17:28:19 +0800

    Beautify the home page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,8 @@
     <div class="container mx-auto h-screen justify-center flex flex-col max-w-xs">
         <div class>
             <div class="bg-blue-500 rounded-full hover:shadow hover:bg-blue-600
-            text-white text-xl mb-5 py-2 px-4">
-                <a href="/game/create">建房</a>
+            text-white text-xl mb-5 py-2 flex justify-center">
+                <a class="" href="/game/create">建房</a>
             </div>
             <form class="flex flex-row justify-between bg-gray-200 rounded-full"
                   method="post" action="/game/enter">
