603025d5a69eda44b38b65e0a7ba18ca086e8a29
HopeWu <767088240@qq.com>
2021-06-01 17:16:02 +0800

    Beautify the home page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,20 @@
 <x-master>
     <div class="container  max-w-md mx-auto h-screen justify-center flex flex-col">
         <div class>
-            <div class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xl">
+            <div class="bg-blue-500 rounded-full shadow py-2 px-4
+            text-white text-xl hover:shadow hover:bg-blue-600">
                 <a href="/game/create">建房</a>
             </div>
 
-            <form class="flex flex-row justify-between"
+            <form class="flex flex-row justify-between bg-gray-200 rounded-full"
                   method="post" action="/game/enter">
                 @csrf
-                <input type="string" name="roomId" class="border border-blue-500 shadow">
-                <button type='submit' class='bg-blue-500 rounded-full shadow py-2 px-4 text-white'>
+                <input class="w-full bg-transparent focus-within:outline-none px-4"
+                       type="string" name="roomId" placeholder="请输入房间编号">
+                <button class='bg-blue-500 rounded-full py-2 px-4 text-white
+                hover:shadow hover:bg-blue-600'
+                        style="min-width:150px;"
+                        type='submit'>
                     进入房间
                 </button >
             </form>
