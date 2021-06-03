1a777a05aa143d48709540489cbe34cf38b62fdc
HopeWu <767088240@qq.com>
2021-06-03 12:28:54 +0800

    enter room

--- app/Http/Controllers/PlayGameController.php
+++ app/Http/Controllers/PlayGameController.php
@@ -70,7 +70,7 @@ class PlayGameController extends Controller
     }
 
     public function shuffle(Request $request){
-        return shuffleNew($request['roomId']);
+        return $this->shuffleNew($request['roomId']);
     }
 
     /**
