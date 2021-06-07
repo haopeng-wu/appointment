eb20f76a91170dcdda8b1824c07e4e7da7356c9d
HopeWu <767088240@qq.com>
2021-06-07 16:26:39 +0800

    add refresh role card in show role page

--- resources/views/game/role.blade.php
+++ resources/views/game/role.blade.php
@@ -1,10 +1,12 @@
 <x-master>
-    <div class="flex flex-col justify-between">
+    <div class="flex flex-col justify-between h-screen">
         <div><h2>你的身份是：<span class="text-red-600">{{$role->chinese_name}}</span>。</h2>
         </div>
-        <div class="bg-blue-500 rounded-lg hover:shadow hover:bg-blue-600
-                    text-white text-xl mb-5 py-2 flex justify-center items-center">
-            <a href="/game/role"><h2>刷新身份</h2></a>
-        </div>
+        <a href="/game/role">
+            <div class="bg-blue-500 rounded-lg hover:shadow hover:bg-blue-600
+                        text-white text-xl mb-5 py-2 flex justify-center items-center">
+                <h2>刷新身份</h2>
+            </div>
+        </a>
     </div>
 </x-master>
