99603b3fa3a1a86d4e8c88c2b8e7c5e1584bcd6d
HopeWu <767088240@qq.com>
2021-06-07 16:34:04 +0800

    add remind to check the roles

--- resources/views/game/playing.blade.php
+++ resources/views/game/playing.blade.php
@@ -1,10 +1,10 @@
 <x-master>
-        <div>
+        <div class="mb-5">
             @foreach($dist as $player_id => $role)
                 <p><span class="text-red-600">{{$player_id}}</span> plays <span class="text-red-600">{{$role}}</span></p>
             @endforeach
         </div>
         <div>
-                <h3>请提醒玩家查看身份或刷新身份！</h3>
+                <h3 class="text-red-700">请提醒玩家查看身份或刷新身份！</h3>
         </div>
 </x-master>
