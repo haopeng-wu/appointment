af0a295fae0d1fbf9e78f68cd9dd921c2010cd21
HopeWu <767088240@qq.com>
2021-06-07 16:10:10 +0800

    add refresh role card in show role page

--- resources/views/game/role.blade.php
+++ resources/views/game/role.blade.php
@@ -1,3 +1,5 @@
 <x-master>
-<h2>你的身份是：<span class="text-red-600">{{$role->chinese_name}}</span>。</h2>
+    <div><h2>你的身份是：<span class="text-red-600">{{$role->chinese_name}}</span>。</h2>
+    </div>
+    <div><a href="/game/role"><h2>刷新身份</h2></a></div>
 </x-master>
