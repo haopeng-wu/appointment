f61a416884b483c78ed5d2572ba9a0d86d4fdce7
HopeWu <767088240@qq.com>
2021-06-10 16:45:55 +0800

    add a button to enter the room on the created page.

--- resources/views/game/created.blade.php
+++ resources/views/game/created.blade.php
@@ -4,7 +4,6 @@
             <h1>房间创建成功！房间号为: <span class="font-bold text-red-700 text-xl rounded">{{$gameId}}</span>。</h1>
         </div>
         <div>
-            <a href=""><h1>进入房间</h1></a>
             <form action="/game/enter"
                   method="post"
             >
