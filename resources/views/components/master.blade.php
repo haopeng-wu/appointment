30476116549c1b7749604e70835a621fb3ced616
HopeWu <767088240@qq.com>
2021-06-11 12:35:04 +0800

    update some eloquent

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <title>Werewolf</title>
     <meta charset="UTF-8">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
