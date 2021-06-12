1c0446bc2cafc20e3c0e673e91a3733fef89e386
HopeWu <767088240@qq.com>
2021-06-12 15:49:03 +0800

    update some eloquent

--- resources/views/game/room.blade.php
+++ resources/views/game/room.blade.php
@@ -1,29 +1,39 @@
 <x-master>
-    <div>
-        <h1>欢迎玩家<span>{{$user->id}}</span>进入房间
-        <span class="text-red-600 font-bold ml-1">{{$room->id}}</span></h1>
+
+    @if(! $room->host or ! $room->host == $user->id)
+        <h1>欢迎玩家<span class="text-red-600 font-bold ml-1">{{$user->id}}</span>进入房间
+            <span class="text-red-600 font-bold ml-1">{{$room->id}}</span></h1>
         <div class="flex">
-            <a href="/game/role" >
+            <a href="/game/role">
                 <div class='bg-blue-500 rounded-full py-2 px-4 text-white
-                hover:shadow hover:bg-blue-600 focus-within:outline-none mr-3'
+                    hover:shadow hover:bg-blue-600 focus-within:outline-none mr-3'
                      style="min-width:100px;"
-                     >
+                >
                     查看身份
                 </div>
             </a>
-            @if($room->host and $room->host == $user->id)
-                <form method="post"
-                      action="/game/shuffle-cards">
-                    @csrf
-                    <input type="hidden" name="_roomId" value="{{$room->id}}">
-                    <button class='bg-blue-500 rounded-full py-2 px-4 text-white
-                    hover:shadow hover:bg-blue-600 focus-within:outline-none'
-                            style="min-width:100px;"
-                            type='submit'>
-                        发身份牌
-                    </button>
-                </form>
-            @endif
+
+            <form action=""></form>
         </div>
-    </div>
+    @endif
+
+    @if($room->host and $room->host == $user->id)
+        <h1>欢迎主持人<span class="text-red-600 font-bold ml-1">{{$user->id}}</span>进入房间
+            <span class="text-red-600 font-bold ml-1">{{$room->id}}</span></h1>
+        <div>
+            <form method="post"
+                  action="/game/shuffle-cards">
+                @csrf
+                <input type="hidden" name="_roomId" value="{{$room->id}}">
+                <button class='bg-blue-500 rounded-full py-2 px-4 text-white
+                        hover:shadow hover:bg-blue-600 focus-within:outline-none'
+                        style="min-width:100px;"
+                        type='submit'>
+                    发身份牌
+                </button>
+            </form>
+        </div>
+    @endif
+
+
 </x-master>
