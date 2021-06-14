e00b0e02e497e9333cd5a318fb980a2bb6700907
HopeWu <767088240@qq.com>
2021-06-14 11:37:43 +0800

    host button

--- app/Http/Controllers/HostController.php
+++ app/Http/Controllers/HostController.php
@@ -18,7 +18,7 @@ class HostController extends Controller
         if((! $room->host) or ($room->set_host_at < Carbon::now()->subMinutes(90))){
             # there is no host yet, this user becomes the first host of this game
             $user->hostGame($room);
-            return "you're a host now";
+            return view("game.room", ["room"=>$room, "user"=>$user]);
         }else{
             # there is a host, who hasn't retired yet, the current user cannot be a host of this game
             return "已经有主持人";
