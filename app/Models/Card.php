6cd92149bf848b67d90dccd5027a3ee9b3e5afa5
HopeWu <767088240@qq.com>
2021-06-18 16:25:44 +0800

    add getPortraitAttribute to card model

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Card extends Model
 {
     use HasFactory;
+
+    public function getPortraitAttribute($value){
+        if($value){
+            return asset('images/identities/'.$value);
+        }else{
+            return null;
+        }
+    }
 }
