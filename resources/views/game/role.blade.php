abd5fa6d99a74d68610167570c619b49d27e1b1e
HopeWu <767088240@qq.com>
2021-06-21 08:39:48 +0800

    adjust role page

--- resources/views/game/role.blade.php
+++ resources/views/game/role.blade.php
@@ -3,7 +3,7 @@
         <div class="hidden"><h2>你的身份是：<span class="text-red-600 mb-3">{{$role->chinese_name}}</span>。</h2>
         </div>
         <div class="">
-            <img src="{{$role->portrait}}" alt="$role->chinese_name">
+            <img class="mx-auto" src="{{$role->portrait}}" alt="$role->chinese_name">
         </div>
         <a href="/game/role">
             <div class="bg-blue-500 rounded-lg hover:shadow hover:bg-blue-600
