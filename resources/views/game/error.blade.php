33a0ce9aa4e9900b8072ed33c7227b7968a85dde
HopeWu <767088240@qq.com>
2021-06-21 08:43:45 +0800

    adjust role page

--- resources/views/game/error.blade.php
+++ resources/views/game/error.blade.php
@@ -1,34 +1,38 @@
 <x-master>
-    @isset($error)
-        <div>
-            <h2 class="text-red-600">{{$error}}</h2>
-        </div>
-    @else
-        <div>
-            <h2 class="text-red-600">您没有输入房间编号！</h2>
-        </div>
-    @endisset
+    <div class="container mx-auto h-screen justify-center flex flex-col max-w-xs">
+        <div class>
+            @isset($error)
+                <div>
+                    <h2 class="text-red-600">{{$error}}</h2>
+                </div>
+            @else
+                <div>
+                    <h2 class="text-red-600">您没有输入房间编号！</h2>
+                </div>
+            @endisset
 
-    <div class="flex">
-        @if(session("room_id"))
-        <a href="/room">
-            <div class='bg-blue-500 rounded-full py-2 px-4 text-white
+            <div class="flex">
+                @if(session("room_id"))
+                    <a href="/room">
+                        <div class='bg-blue-500 rounded-full py-2 px-4 text-white
                     hover:shadow hover:bg-blue-600 focus-within:outline-none mr-2
                     flex justify-center'
-                 style="min-width:100px;"
-            >
-                返回房间
-            </div>
-        </a>
-        @endif
-        <a href="/">
-            <div class='bg-blue-500 rounded-full py-2 px-4 text-white
+                             style="min-width:100px;"
+                        >
+                            返回房间
+                        </div>
+                    </a>
+                @endif
+                <a href="/">
+                    <div class='bg-blue-500 rounded-full py-2 px-4 text-white
                     hover:shadow hover:bg-blue-600 focus-within:outline-none
                     flex justify-center'
-                 style="min-width:100px;"
-            >
-                返回首页
+                         style="min-width:100px;"
+                    >
+                        返回首页
+                    </div>
+                </a>
             </div>
-        </a>
+        </div>
     </div>
 </x-master>
