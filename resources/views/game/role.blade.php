698778c7cdb56f52db59731bf872cba64d5cc43d
HopeWu <767088240@qq.com>
2021-06-22 21:08:50 +0800

    add a background image to the home page

--- resources/views/game/role.blade.php
+++ resources/views/game/role.blade.php
@@ -5,7 +5,7 @@
         <div class="">
             <img class="mx-auto" src="{{$role->portrait}}" alt="$role->chinese_name">
         </div>
-        <a href="/game/role">
+        <a href="/role">
             <div class="bg-blue-500 rounded-lg hover:shadow hover:bg-blue-600
                         text-white text-xl mb-5 py-2 flex justify-center">
                 <h2>刷新身份</h2>
