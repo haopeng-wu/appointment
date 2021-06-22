ab90bc395cd98d0cd4fbbf4909868f65bf976afc
HopeWu <767088240@qq.com>
2021-06-22 21:42:59 +0800

    add a background image to the role page

--- resources/views/game/role.blade.php
+++ resources/views/game/role.blade.php
@@ -1,16 +1,19 @@
 <x-master>
-    <div class="">
+    <div class="" style="background-image: url({!! $role->portrait !!})">
         <div class="hidden"><h2>你的身份是：<span class="text-red-600 mb-3">{{$role->chinese_name}}</span>。</h2>
         </div>
 
+        <!--
         <div class="">
             <img class="mx-auto" src="{{$role->portrait}}" alt="$role->chinese_name"
                  style="object-fit: cover">
         </div>
+        -->
 
         <a href="/role">
             <div class="bg-blue-500 hover:shadow hover:bg-blue-600
-                        text-white text-xl mb-5 py-2 flex justify-center">
+                        text-white text-xl mb-5 py-2 flex justify-center"
+                 style="position: fixed; bottom: 0;">
                 <h2>刷新身份</h2>
             </div>
         </a>
