0e5a1514c05ce996c71b7073f908c83b4de4d84e
HopeWu <767088240@qq.com>
2021-06-25 17:14:50 +0800

    test

--- resources/views/game/role.blade.php
+++ resources/views/game/role.blade.php
@@ -2,9 +2,9 @@
     <div class="h-screen">
         <div class="hidden"><h2>你的身份是：<span class="text-red-600 mb-3">{{$role->chinese_name}}</span>。</h2>
         </div>
-
+        <div>
             <img class="mx-auto" src="{!! $role->portrait !!}" alt="{{$role->chinese_name}}">
-
+        </div>
         <a href="/role">
             <div class="bg-blue-500 hover:shadow hover:bg-blue-600
                         text-white text-xl mb-5 py-2 flex justify-center">
