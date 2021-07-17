c7e57b4f6fc0e14d0b1cc045b05faf1987833642
HopeWu <767088240@qq.com>
2021-07-17 16:58:13 +0800

    adjust home page

--- resources/views/game/room.blade.php
+++ resources/views/game/room.blade.php
@@ -1,7 +1,7 @@
 <x-master>
 
     <div style="display: grid; grid-template-rows: repeat(10, 1fr)">
-        <div style="grid-row:1/10; background-image: url({!! asset('images/u=2463211854,2553144130&fm=26&gp=0.jpeg') !!});
+        <div style="grid-row:1/10; background-image: url({!! asset('images/src=http---img7.d.cn-be-image-1711-sh447j9qodqsp.jpg&refer=http---img7.d.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg.jpg') !!});
                 background-repeat: no-repeat; background-position: center;
                 background-size: cover;">
             <!--
