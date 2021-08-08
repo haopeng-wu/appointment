dafdaecab422477184dd084af5db583b13d37d77
HopeWu <767088240@qq.com>
2021-08-08 17:45:23 +0800

    adjust room page

--- resources/views/game/room.blade.php
+++ resources/views/game/room.blade.php
@@ -1,38 +1,42 @@
 <x-master>
 
-    <div style="display: grid; grid-template-rows: repeat(10, 1fr)">
-        <div style="grid-row:1/10; background-image: url({!! asset('images/u=304344891,2514493180&fm=26&gp=0.jpeg') !!});
-                background-repeat: no-repeat; background-position: center;
-                background-size: cover;">
-            <!--
-            <img src="{!! asset('images/u=2463211854,2553144130&fm=26&gp=0.jpeg') !!}" alt=""
-                 style="object-fit: cover; width:100% ;grid-row: 1/10">
-                 -->
-        </div>
-        <div style="grid-row: 10/11">
+    <div style="height: 100vh;
+            background-image: url({!! asset('images/u=304344891,2514493180&fm=26&gp=0.jpeg') !!});
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: cover;
+            display: grid;
+            grid-template-rows: repeat(10, 1fr);
+            grid-template-columns: repeat(10, 1fr);
+            ">
+        <div class="container lg:mx-auto justify-center flex flex-col lg:max-w-xs"
+             style="grid-row: 6/7; grid-column: 3/9; border: solid; border: #9561e2"
+        >
             @if((! $room->host) or $room->host != $user)
                 <h1>欢迎玩家<span class="text-red-600 font-bold ml-1 mr-3">{{$user->id}}</span>进入房间
                     <span class="text-red-600 font-bold ml-1">{{$room->id}}</span></h1>
-                <div class="flex">
-                    <a href="/role">
-                        <div class='bg-blue-500 rounded-full py-2 px-4 text-white
-                    hover:shadow hover:bg-blue-600 focus-within:outline-none mr-3'
-                             style="min-width:100px;"
-                        >
-                            查看身份
+                <div>
+                    <a class="" href="/role">
+                        <div class="rounded-lg hover:shadow hover:bg-blue-600
+                mb-5 py-2 flex justify-center items-center"
+                             style="height:100%; width: 100%; background: rgba(5, 17, 250, 0.5);">
+                            <span style="color: white; font-size: 1.25rem; line-height: 1.75rem;">查看身份</span>
                         </div>
                     </a>
-
-                    <form action="/game/host"
+                    <form class="flex flex-row justify-between bg-gray-200 rounded-lg"
                           method="post"
-                    >
+                          action="/game/host"
+                          style="height:100%; width: 100%; background: rgba(255, 255, 255, 0.5);">
                         @csrf
-                        <input type="hidden" name="_roomId" value="{{$room->id}}">
-                        <button class='bg-blue-500 rounded-full py-2 px-4 text-white
-                        hover:shadow hover:bg-blue-600 focus-within:outline-none'
-                                style="min-width:100px;"
+                        <input class="w-full focus-within:outline-none px-2"
+                               type="number" name="roomId" placeholder="请输入房间号"
+                               required
+                               style="background: rgba(255, 255, 255, 0.5);">
+                        <button class='rounded-md py-2 px-4 text-white
+                hover:shadow hover:bg-blue-600 focus-within:outline-none'
+                                style="min-width:100px; background: rgba(5, 17, 250, 0.5);"
                                 type='submit'>
-                            主持游戏
+                            <span style="color: white;">主持游戏</span>
                         </button>
                     </form>
                 </div>
