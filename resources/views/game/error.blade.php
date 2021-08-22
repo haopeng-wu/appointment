e5d6dccc23b2d4a0afbed9bdc2c267ffff3e5278
HopeWu <767088240@qq.com>
2021-08-22 16:43:36 +0800

    adjust error page

--- resources/views/game/error.blade.php
+++ resources/views/game/error.blade.php
@@ -21,20 +21,20 @@
         <div style="display: flex;">
             @if(session("room_id"))
                 <a href="/room">
-                    <div class='bg-blue-500 rounded-full py-2 px-4 text-white
+                    <div class='bg-blue-500 py-2 px-4 text-white
                     hover:shadow hover:bg-blue-600 focus-within:outline-none mr-2
                     flex justify-center'
-                         style="min-width:100px;"
+                         style="min-width:100px; border-radius:0.5em;"
                     >
                         返回房间
                     </div>
                 </a>
             @endif
             <a href="/">
-                <div class='bg-blue-500 rounded-full py-2 px-4 text-white
+                <div class='bg-blue-500 py-2 px-4 text-white
                     hover:shadow hover:bg-blue-600 focus-within:outline-none
                     flex justify-center'
-                     style="min-width:100px;"
+                     style="min-width:100px; border-radius:0.5em;"
                 >
                     返回首页
                 </div>
