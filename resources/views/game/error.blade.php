5a533844471ae57446bc3548cefc9c421ca50a89
HopeWu <767088240@qq.com>
2021-08-22 16:56:35 +0800

    adjust error page

--- resources/views/game/error.blade.php
+++ resources/views/game/error.blade.php
@@ -17,28 +17,30 @@
             @else
                 <h2>您没有输入房间编号！</h2>
             @endisset
-        </div>
-        <div style="display: flex;">
-            @if(session("room_id"))
-                <a href="/room">
-                    <div class='bg-blue-500 py-2 px-4 text-white
+
+            <div style="display: flex; justify-content: space-between;">
+                @if(session("room_id"))
+                    <a href="/room">
+                        <div class='bg-blue-500 py-2 px-4 text-white
                     hover:shadow hover:bg-blue-600 focus-within:outline-none mr-2
+                    flex justify-center rounded-md'
+                             style="min-width:100px;"
+                        >
+                            返回房间
+                        </div>
+                    </a>
+                @endif
+                <a href="/">
+                    <div class='bg-blue-500 py-2 px-4 text-white
+                    hover:shadow hover:bg-blue-600 focus-within:outline-none
                     flex justify-center rounded-md'
                          style="min-width:100px;"
                     >
-                        返回房间
+                        返回首页
                     </div>
                 </a>
-            @endif
-            <a href="/">
-                <div class='bg-blue-500 py-2 px-4 text-white
-                    hover:shadow hover:bg-blue-600 focus-within:outline-none
-                    flex justify-center rounded-md'
-                     style="min-width:100px;"
-                >
-                    返回首页
-                </div>
-            </a>
+            </div>
         </div>
+
     </div>
 </x-master>
