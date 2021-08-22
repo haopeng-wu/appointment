dc2b1eb3e6b950246419aec36d4b0a36b0c5c3ff
HopeWu <767088240@qq.com>
2021-08-22 11:00:31 +0800

    adjust error page

--- resources/views/game/error.blade.php
+++ resources/views/game/error.blade.php
@@ -8,37 +8,37 @@
         </div>
         <div
 
-                 style="font-size: 2em;
+                style="font-size: 2em;
                         color: red;
-                        display: flex; justify-content: space-around; margin-top:1em; margin-bottom: 1em;">
-                @isset($error)
-                    <h2 style="font-family:'xiaowanzi';">{{$error}}</h2>
-                @else
-                    <h2>您没有输入房间编号！</h2>
-                @endisset
-            </div>
-            <div style="display: flex;">
-                @if(session("room_id"))
-                    <a href="/room">
-                        <div class='bg-blue-500 rounded-full py-2 px-4 text-white
-                    hover:shadow hover:bg-blue-600 focus-within:outline-none mr-2
-                    flex justify-center'
-                             style="min-width:100px;"
-                        >
-                            返回房间
-                        </div>
-                    </a>
-                @endif
-                <a href="/">
+                        display: flex; justify-content: space-around; margin-top:1em; margin-bottom: 1em;"
+        >
+            @isset($error)
+                <h2 style="font-family:'Microsoft JhengHei';">{{$error}}</h2>
+            @else
+                <h2>您没有输入房间编号！</h2>
+            @endisset
+        </div>
+        <div style="display: flex;">
+            @if(session("room_id"))
+                <a href="/room">
                     <div class='bg-blue-500 rounded-full py-2 px-4 text-white
-                    hover:shadow hover:bg-blue-600 focus-within:outline-none
+                    hover:shadow hover:bg-blue-600 focus-within:outline-none mr-2
                     flex justify-center'
                          style="min-width:100px;"
                     >
-                        返回首页
+                        返回房间
                     </div>
                 </a>
-            </div>
+            @endif
+            <a href="/">
+                <div class='bg-blue-500 rounded-full py-2 px-4 text-white
+                    hover:shadow hover:bg-blue-600 focus-within:outline-none
+                    flex justify-center'
+                     style="min-width:100px;"
+                >
+                    返回首页
+                </div>
+            </a>
         </div>
     </div>
 </x-master>
