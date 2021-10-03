de4e3b17a7616ebb3d9ba89a5d34a3c0e0f79e1c
HopeWu <767088240@qq.com>
2021-10-03 22:32:32 +0800

    home page radio select js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,5 @@
 //let slots = document.querySelectorAll("input[name='time-slot']");
 let slots = document.querySelector(".time-slots");
-slots.addEventListener("selected", function(event){
+slots.addEventListener("checked", function(event){
     console.log(event.target);
 })
