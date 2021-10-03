8357c78ccae7472f0fc4fd80ccb7309c89ce1a28
HopeWu <767088240@qq.com>
2021-10-03 22:50:31 +0800

    home page radio js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,9 +1,16 @@
 console.log("hello");
+
+/*
 let slots = document.querySelectorAll("input[name='time-slot']");
-//let slots = document.querySelector(".time-slots");
 
 for(let i = 0; i < slots.length; i++) {
     slots[i].addEventListener("change", function (event) {
         console.log(event.target);
     })
 }
+*/
+
+let slots = document.querySelector(".time-slots");
+slots.addEventListener("change", function (event) {
+    console.log(event.target);
+})
