40f0177efae0b89594cf469b260368d5bf9f7af9
HopeWu <767088240@qq.com>
2021-10-03 22:31:08 +0800

    home page radio select js

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <title>appointment</title>
     <meta charset="UTF-8">
-    <link href="{{ asset('css/custom.css')}}v={{getdate()['mon'].getdate()['mday'].getdate()['hours'].getdate()['minutes']}}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css')}}?v={{getdate()['mon'].getdate()['mday'].getdate()['hours'].getdate()['minutes']}}" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
