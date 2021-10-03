a934638a90b0e7d3323c171d8d62f53e69e7ce35
HopeWu <767088240@qq.com>
2021-10-03 12:32:58 +0800

    first commit. clean up

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -1,30 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Werewolf</title>
+    <title>appointment</title>
     <meta charset="UTF-8">
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css')}}v={{getdate()['mon'].getdate()['mday'].getdate()['hours'].getdate()['minutes']}}" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <script type="text/javascript" src="https://cdn.repository.webfont.com/wwwroot/js/wf/youziku.api.min.js"></script>
-    <script type="text/javascript">
-        $webfont.load("body", "1a4b055e3dbd404dbc9a757c00e5623b", "ruibo");
-        /*$webfont.load("#id1,.class1,h1", "1a4b055e3dbd404dbc9a757c00e5623b", "ruibo");*/
-        /*．．．*/
-        $webfont.draw();
-    </script>
 </head>
-<body class="">
-    <div class="bg-blue-300 flex justify-between"
-    style="height: 1.75rem; line-height: 1.75rem;">
-        <a class="flex" href="/"><h2>Home</h2></a>
-        @if($user_id = session('user_id'))
-            <h2 class="flex">您的编号是 <span class="text-red-700 font-bold">{{$user_id}}</span>
-                @if($roomId=session('room_id'))，当前活跃房间号为<span class="text-red-700 font-bold">{{$roomId}}</span>@endif
-            </h2>
-        @endif
-        <h2 class="flex">Werewolf</h2>
-    </div>
+<body>
     {{$slot}}
 </body>
 </html>
--- resources/views/layouts/app.php
+++
@@ -1,6 +0,0 @@
-<!doctype html>
-  <head>
-   <meta charset="UTF-8" />
-   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-  </head>
