a90284b6794d88cfb1e8430b5b80a97a83e093b8
HopeWu <767088240@qq.com>
2021-10-05 11:30:58 +0800

    appointments table and users table migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->string("gender")->nullable();
+            $table->string("sex_orientation")->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string("tel")->nullable();
