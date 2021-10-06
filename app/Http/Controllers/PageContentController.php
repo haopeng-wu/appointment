68fca0abfcedfc068ff9245d7708ba99a134a59c
HopeWu <767088240@qq.com>
2021-10-06 10:14:24 +0800

    page contents

---
+++ app/Http/Controllers/PageContentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PageContent;
+use Illuminate\Http\Request;
+
+class PageContentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PageContent  $pageContent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PageContent $pageContent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PageContent  $pageContent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PageContent $pageContent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PageContent  $pageContent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PageContent $pageContent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PageContent  $pageContent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PageContent $pageContent)
+    {
+        //
+    }
+}
