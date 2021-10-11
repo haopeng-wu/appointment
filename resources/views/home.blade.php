be50a020e221b54e1a7712e6f7cfdb84ff91a591
Hope <haopeng_wu@qq.com>
2021-10-11 22:38:56 +0800

    add form error indicator

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,18 +14,30 @@
                         <div class="time-slots">
                             <div>
                                 <input class="time_slot" type="radio" id="slot-1" name="which_slot" value="1">
+                                @error("which_slot")
+                                <p>{{$message}}</p>
+                                @enderror
                                 <label class="unchecked" for="slot-1">08:30~10:00</label>
                             </div>
                             <div>
                                 <input class="time_slot" type="radio" id="slot-2" name="which_slot" value="2">
+                                @error("which_slot")
+                                <p>{{$message}}</p>
+                                @enderror
                                 <label class="unchecked" for="slot-2">10:30~12:00</label>
                             </div>
                             <div>
                                 <input class="time_slot" type="radio" id="slot-3" name="which_slot" value="3">
+                                @error("which_slot")
+                                <p>{{$message}}</p>
+                                @enderror
                                 <label class="unchecked" for="slot-3">14:30~16:00</label>
                             </div>
                             <div>
                                 <input class="time_slot" type="radio" id="slot-4" name="which_slot" value="4">
+                                @error("which_slot")
+                                <p>{{$message}}</p>
+                                @enderror
                                 <label class="unchecked" for="slot-4">16:30~18:00</label>
                             </div>
                         </div>
@@ -39,8 +51,17 @@
                             </div>
                             <div class="inputs">
                                 <input name="customer_name" type="text" id="customer_name">
+                                @error("customer_name")
+                                <p>{{$message}}</p>
+                                @enderror
                                 <input name="email" type="email" id="email">
+                                @error("email")
+                                <p>{{$message}}</p>
+                                @enderror
                                 <input name="tel" type="text" id="tel">
+                                @error("tel")
+                                <p>{{$message}}</p>
+                                @enderror
                             </div>
                         </div>
                     </article>
