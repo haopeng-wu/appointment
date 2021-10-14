b808708148db14f340385adeca6f038160fd15f7
Hope <haopeng_wu@qq.com>
2021-10-14 14:02:36 +0800

    use a redirect to the checkout page

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -112,11 +112,16 @@ class AppointmentController extends Controller
          * flush these to the session to use it after the redirection
          */
 
-        $request->session()->flash([
+        /*
+         * $request->session()->flash([
             'appt' => $appointment,
             'start_end' => $start_end,
             'html_snippet' => $html_snippet
         ]);
+         */
+
+        $request->session()->flash('appt', $appointment);
+
         //dd($request->session()->all());
 
         return redirect('/checkout');
