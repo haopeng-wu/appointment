b9ac8e520f203b37cbf0ef53f444dd0505c8c237
Hope <haopeng_wu@qq.com>
2021-10-15 18:29:18 +0800

    admin page fix

--- resources/views/admin-schedules.blade.php
+++ resources/views/admin-schedules.blade.php
@@ -1,8 +1,8 @@
 <x-a-master>
     <div class="search-bar wrapper">
+        <input type="text" name="search" placeholder="enter an email or a date">
         <div class="icon">
             <img src="{{asset('images/search_icon.png')}}" alt="search icon">
         </div>
-        <input type="text" name="search" placeholder="enter an email or a date">
     </div>
 </x-a-master>
