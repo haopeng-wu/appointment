c47b25a664b63ce10921d8bd2990c52024dc447b
Hope <haopeng_wu@qq.com>
2021-10-15 18:05:03 +0800

    admin page nav border

--- resources/views/admin-schedules.blade.php
+++ resources/views/admin-schedules.blade.php
@@ -1,7 +1,7 @@
 <x-a-master>
     <div class="search-bar wrapper">
         <div class="icon">
-            <img src="{{asset('images/search-icon.png')}}" alt="search icon">
+            <img src="{{asset('images/search_icon.png')}}" alt="search icon">
         </div>
         <div class="input">
             <input type="text" name="search" placeholder="enter an email or a date">
