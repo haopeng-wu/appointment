e07252d16c0c8bef697584e83139448a477e5f0d
Hope <haopeng_wu@qq.com>
2021-10-15 18:01:50 +0800

    admin page fix bug

--- resources/views/admin-dashboard.blade.php
+++ resources/views/admin-dashboard.blade.php
@@ -1,10 +1,3 @@
-<a-master>
-<div class="search-bar wrapper">
-    <div class="icon">
-        <img src="{{asset('images/search-icon.png')}}" alt="search icon">
-    </div>
-    <div class="input">
-        <input type="text" name="search" placeholder="enter an email or a date">
-    </div>
-</div>
-</a-master>
+<x-a-master>
+
+</x-a-master>
