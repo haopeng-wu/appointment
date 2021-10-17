3660a7ecf95cdc6d3458871ea0429a248db0686b
Hope <haopeng_wu@qq.com>
2021-10-17 11:22:16 +0800

    mirate the slot table and seed it

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 		$this->call([
-			CardSeeder::class,
+			SlotSeeder::class,
 		]);
         // \App\Models\User::factory(10)->create();
     }
