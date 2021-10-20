a26992913422095402cd72b9637856abb0e47039
Hope <haopeng_wu@qq.com>
2021-10-20 11:41:11 +0800

    dashboard configure slots

--- public/js/admin-custom.js
+++ public/js/admin-custom.js
@@ -1,11 +1,11 @@
 const firstTable = document.querySelector('form:first-of-type table');
 const thead = document.createDocumentFragment(
-    "<thead>\n" +
-    "    <tr>\n" +
-    "        <th>Start at</th>\n" +
-    "        <th>End by</th>\n" +
-    "        <th>Price</th>\n" +
-    "        <th>Modify</th>\n" +
-    "    </tr>\n" +
+    "<thead>" +
+    "    <tr>" +
+    "        <th>Start at</th>" +
+    "        <th>End by</th>" +
+    "        <th>Price</th>" +
+    "        <th>Modify</th>" +
+    "    </tr>" +
     "    </thead>");
 firstTable.prepend(thead);
