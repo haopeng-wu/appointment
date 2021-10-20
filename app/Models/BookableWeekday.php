3742396f8a5631fe0b7c066381559cedd1582c3d
Hope <haopeng_wu@qq.com>
2021-10-20 22:33:05 +0800

    dashboard configure slots

--- app/Models/BookableWeekday.php
+++ app/Models/BookableWeekday.php
@@ -6,16 +6,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use phpDocumentor\Reflection\Types\Self_;
 
+function minusOne($n){
+    return ($n - 1);
+}
+
 class BookableWeekday extends Model
 {
     use HasFactory;
     protected $guarded = [];
 
+
+
     static public function getBookableIdMinusOne(){
         $ids = self::all()->where('is_bookable','=', 1)->pluck('id');
-        function minusOne($n){
-            return ($n - 1);
-        }
+
         $ids = array_map('minusOne', $ids);
         return $ids;
     }
