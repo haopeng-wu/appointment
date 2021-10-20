fe69fa4da56b747db6b56c0a0253c1105492ffd3
Hope <haopeng_wu@qq.com>
2021-10-20 10:34:14 +0800

    dashboard configure slots

--- app/Http/Controllers/SlotController.php
+++ app/Http/Controllers/SlotController.php
@@ -60,9 +60,10 @@ class SlotController extends Controller
      */
     public function update(Request $request)
     {
+        dd($request);
         $attributes = request()->validate([
-            'start_at'=>['required','date_format:%H:%i'],
-            'end_at'=>['required','date_format:%H:%i'],
+            'start_at'=>['required','date_format:H:i'],
+            'end_at'=>['required','date_format:H:i'],
             'price'=>['required', 'numeric', 'min:100'],
             'id'=>['required', 'numeric', 'min:1']
         ]);
