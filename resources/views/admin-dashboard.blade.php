81ceab1e199912bec0a35a554ec1febe8c839425
Hope <haopeng_wu@qq.com>
2021-10-21 13:59:44 +0800

    dashboard configure slots

--- resources/views/admin-dashboard.blade.php
+++ resources/views/admin-dashboard.blade.php
@@ -67,7 +67,7 @@
         </div>
     </article>
     <script>
-        {{dd(json_encode($bookableFlags)}}
+        {{dd(json_encode($bookableFlags))}}
         const bookableFlags = {{json_encode($bookableFlags)}};
     </script>
 </x-a-master>
