1e9f613d1238c2d646f7b55502784ca5f3399208
Hope <haopeng_wu@qq.com>
2021-10-21 17:45:08 +0800

    dashboard configure slots

--- public/js/admin-custom.js
+++ public/js/admin-custom.js
@@ -15,7 +15,9 @@ for(let i = 0; i < bookableFlags.length; i++){
     let theInput = document.querySelector(`.weekdays input#${'d'+(i+1)}`);
     //let theLable = document.querySelector(`.weekdays label[for=${i+1}]`);
     //theInput.setAttribute('value', bookableFlags[i+1]);
-    if (bookableFlags[i+1]===1){
+    if (bookableFlags[i+1]==='1'){
         theInput.setAttribute('checked', 1);
     }
 }
+
+// let theInput = document.querySelector(`.weekdays input#${'d'+(0+1)}`);
