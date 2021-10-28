546d8401a508b386135a241147f07efc4fa14801
Hope <haopeng_wu@qq.com>
2021-10-28 12:25:43 +0800

    admin page

--- resources/views/components/a-master.blade.php
+++ resources/views/components/a-master.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>appointment</title>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <link href="{{ asset('css/admin-custom.css')}}?v={{getdate()['mon'].getdate()['mday'].getdate()['hours'].getdate()['minutes']}}"
           rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
