4afd0ed605b759c0201373cf628bfbecf1c231d3
Hope <haopeng_wu@qq.com>
2021-11-01 13:52:08 +0800

    change theme of flatpickr

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -37,9 +37,9 @@
 <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 
 <script src="https://npmcdn.com/flatpickr/dist/flatpickr.min.js"></script>
-<script type="text/javascript" src="{{asset('js/flatpickr-se.js')}}"></script>
-<!--
 
+<!--
+<script type="text/javascript" src="{{asset('js/flatpickr-se.js')}}"></script>
 <script src="https://npmcdn.com/flatpickr/dist/l10n/ru.js"></script>
 -->
 
