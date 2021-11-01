48fe7d387ee41ae5c04e0d5a9a87e3495fb8d40b
Hope <haopeng_wu@qq.com>
2021-11-01 13:17:34 +0800

    change theme of flatpickr

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -8,7 +8,7 @@
     <!--
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     -->
-    <link rel="stylesheet" type="text/css" href="https://npmcdn.com/flatpickr/dist/themes/dark.css">
+    <link rel="stylesheet" type="text/css" href="https://npmcdn.com/flatpickr/dist/themes/airbnb.css">
     <link rel="icon" href="{{asset('images/icon.svg')}}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
