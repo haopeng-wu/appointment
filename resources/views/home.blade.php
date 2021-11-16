d7d6edb4ffdb441fad5b39b609a687322cd63d52
Hope <haopeng_wu@qq.com>
2021-11-16 22:41:13 +0800

    form page content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,33 +3,33 @@
         <section class="center-pic">
             <img src="{{asset('images/home-center.png')}}" alt="home cozy picture">
         </section>
-        <article class="appointment">
-            <form method="post" class="appointment" action="/appointment">
+        <form method="post" class="appointment" action="/appointment">
+            <article class="appointment">
                 @csrf
-                    <div class="schedule">
-                        <div class="time-slots">
-                            <div class="date">
-                                <input id="date" placeholder="Select a date" name="date" type="date"
-                                       value="{{old('date')}}">
-                                @error("date")
-                                <p class="error">{{$message}}</p>
-                                @enderror
-                            </div>
-                            @foreach($slots as $key => $slot)
-                                <div>
-                                    <input class="time_slot" type="radio" id="{{'slot-'.$key}}"
-                                           name="which_slot" value="{{$key}}">
-                                    <label class="" for="{{'slot-'.$key}}">{{$slot}}</label>
-                                </div>
-                            @endforeach
-                            @error("which_slot")
+                <div class="schedule">
+                    <div class="time-slots">
+                        <div class="date">
+                            <input id="date" placeholder="Select a date" name="date" type="date"
+                                   value="{{old('date')}}">
+                            @error("date")
                             <p class="error">{{$message}}</p>
                             @enderror
                         </div>
-                        <button type="submit">Make the Appointment</button>
+                        @foreach($slots as $key => $slot)
+                            <div>
+                                <input class="time_slot" type="radio" id="{{'slot-'.$key}}"
+                                       name="which_slot" value="{{$key}}">
+                                <label class="" for="{{'slot-'.$key}}">{{$slot}}</label>
+                            </div>
+                        @endforeach
+                        @error("which_slot")
+                        <p class="error">{{$message}}</p>
+                        @enderror
                     </div>
-            </form>
-        </article>
+                </div>
+            </article>
+            <button type="submit">Make the Appointment</button>
+        </form>
 
         <section class="promote">
         </section>
