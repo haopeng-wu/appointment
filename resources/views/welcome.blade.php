3c4ed5c8b3b80da334f36bd27d7e0adb32b83fde
Hope <haopeng_wu@qq.com>
2021-11-16 17:35:57 +0800

    welcome page content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,101 +1,105 @@
 <x-master>
-    <article class="promote">
-        <header>
-            <h1>Serious about saving or improving your relationship?</h1>
-        </header>
-        <div>
-            Get professional therapy from a licensed therapist
-            anytime anywhere at the cozy of your home online.
-            <button class="go-to-form">
-                Get Started
-            </button>
-        </div>
-    </article>
-    <article class="promote">
-        <header>
-            <h1>
-                Familjerådgivning/parterapi
-            </h1>
-        </header>
-        <div>
-            <ul>
-                <li>Svårt att prata med varandra</li>
-                <li>Upptrappade konflikter eller långvarig tystnad mellan er</li>
-                <li>Familjebildningproblematik</li>
-                <li>Känslorna har förändrats</li>
-                <li>Någon av er har träffat en annan</li>
-                <li>Otrohet</li>
-                <li>Tankar på separation</li>
-                <li>Sexuella svårigheter</li>
-                <li>Svårt få ihop sin bonusfamilj</li>
-                <li>Svårigheter i relationen mellan vuxna syskon, till en förälder, svärförälder</li>
-            </ul>
-        </div>
-    </article>
+    <main class="landing-page">
+        <article class="promote">
+            <header>
+                <h1>Serious about saving or improving your relationship?</h1>
+            </header>
+            <div>
+                <p>
+                    Get professional therapy from a licensed therapist
+                    anytime anywhere at the cozy of your home online.
+                </p>
+                <button class="go-to-form">
+                    Get Started
+                </button>
+            </div>
+        </article>
+        <article class="promote">
+            <header>
+                <h1>
+                    Familjerådgivning/parterapi
+                </h1>
+            </header>
+            <div>
+                <ul>
+                    <li>Svårt att prata med varandra</li>
+                    <li>Upptrappade konflikter eller långvarig tystnad mellan er</li>
+                    <li>Familjebildningproblematik</li>
+                    <li>Känslorna har förändrats</li>
+                    <li>Någon av er har träffat en annan</li>
+                    <li>Otrohet</li>
+                    <li>Tankar på separation</li>
+                    <li>Sexuella svårigheter</li>
+                    <li>Svårt få ihop sin bonusfamilj</li>
+                    <li>Svårigheter i relationen mellan vuxna syskon, till en förälder, svärförälder</li>
+                </ul>
+            </div>
+        </article>
 
-    <button class="go-to-form">
-        Get Started
-    </button>
+        <button class="go-to-form">
+            Get Started
+        </button>
 
-    <article>
-        <header>
-            <h1>
-                Krisbearbetning
-            </h1>
-        </header>
-        <div>
-            <ul>
-                <li>Ålders/utveckligskris</li>
-                <li>Depression</li>
-                <li>Allvarlig fysisk sjukdom</li>
-                <li>Separation</li>
-                <li>Anhörigs död</li>
-            </ul>
-        </div>
-    </article>
+        <article>
+            <header>
+                <h1>
+                    Krisbearbetning
+                </h1>
+            </header>
+            <div>
+                <ul>
+                    <li>Ålders/utveckligskris</li>
+                    <li>Depression</li>
+                    <li>Allvarlig fysisk sjukdom</li>
+                    <li>Separation</li>
+                    <li>Anhörigs död</li>
+                </ul>
+            </div>
+        </article>
 
-    <button class="go-to-form">
-        We're Ready
-    </button>
+        <button class="go-to-form">
+            We're Ready
+        </button>
 
-    <article>
-        <header>
-            <h1>Samtalsterapi/KBT/fobiträning</h1>
-        </header>
-        <div>
-            <ul>
-                <li>Bearbetning av känslor och tankemönster som leder till oönskade</li>
-                <li>reaktioner, handlingar, undvikanden.</li>
-                <li>Aggressionsproblematik</li>
-                <li>Olika former av fobier</li>
-            </ul>
-        </div>
-    </article>
+        <article>
+            <header>
+                <h1>Samtalsterapi/KBT/fobiträning</h1>
+            </header>
+            <div>
+                <ul>
+                    <li>Bearbetning av känslor och tankemönster som leder till oönskade</li>
+                    <li>reaktioner, handlingar, undvikanden.</li>
+                    <li>Aggressionsproblematik</li>
+                    <li>Olika former av fobier</li>
+                </ul>
+            </div>
+        </article>
 
-    <article>
-        <header>
-            <h1>Utbildning</h1>
-        </header>
-        <div>
-            <ul>
-                <li>Socionom</li>
-                <li>Kognitiv psykoterapi</li>
-                <li>Sexologi</li>
-            </ul>
-        </div>
-    </article>
+        <article>
+            <header>
+                <h1>Utbildning</h1>
+            </header>
+            <div>
+                <ul>
+                    <li>Socionom</li>
+                    <li>Kognitiv psykoterapi</li>
+                    <li>Sexologi</li>
+                </ul>
+            </div>
+        </article>
 
-    <article>
-        <header>
-            <h1>Lång arbetslivserfarenhet som</h1>
-        </header>
-        <div>
-            <ul>
-                <li>Behandlare beroendeproblematik</li>
-                <li>Socionom slutenvårdspsykiatri</li>
-                <li>Kurator på vårdcentral</li>
-                <li>Auktoriserad familjerådgivare</li>
-            </ul>
-        </div>
-    </article>
+        <article>
+            <header>
+                <h1>Lång arbetslivserfarenhet som</h1>
+            </header>
+            <div>
+                <ul>
+                    <li>Behandlare beroendeproblematik</li>
+                    <li>Socionom slutenvårdspsykiatri</li>
+                    <li>Kurator på vårdcentral</li>
+                    <li>Auktoriserad familjerådgivare</li>
+                </ul>
+            </div>
+        </article>
+    </main>
 </x-master>
