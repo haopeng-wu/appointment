d56f58a7e8ed5c00b514e9303195afd0abc61727
HopeWu <767088240@qq.com>
2021-12-05 11:36:35 +0800

    make improvement in appointment-details.blade.php in rel-105

--- resources/views/appointment-details.blade.php
+++ resources/views/appointment-details.blade.php
@@ -4,9 +4,24 @@
             <h4>
                 Details of this appointment
             </h4>
-            <pre>
-                {{print_r($appointment)}}
-            </pre>
+
+            <table>
+                <thead>
+
+                </thead>
+                <tbody>
+                @foreach($appointment as $key => $value)
+                    <tr>
+                        <td>
+                            {{$key}}
+                        </td>
+                        <td>
+                            {{$value}}
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </section>
     </main>
 </x-a-master>
