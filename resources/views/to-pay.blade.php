69d1af2baac07cdb8852e549a30c0ac9a966317a
HopeWu <767088240@qq.com>
2021-12-07 17:21:14 +0800

    rel-100 Translate meeting time text from English to Swedish

--- resources/views/to-pay.blade.php
+++ resources/views/to-pay.blade.php
@@ -1,13 +1,19 @@
 <x-master>
-    <article>
+    <article class="confirm">
         <h2>
-
+            <!--
+            Please check the appointment before you continue to make the payment
+            -->
+            Vänligen kontrollera mötet innan du fortsätter att göra betalningen.
         </h2>
         <div>
             <p>
-                On {{$appt->date}}, from {{$start_end[0]}} to {{$start_end[1]}}, through Google Meeting.
+                <!--On {{$appt->date}}, from {{$start_end[0]}} to {{$start_end[1]}}, through ZOOM.
+                -->
+                Den {{$appt->date}}, från kl {{$start_end[0]}} till kl {{$start_end[1]}}, via ZOOM.
             </p>
-            {!! $html_snippet !!}
         </div>
     </article>
+
+    {!! $html_snippet !!}
 </x-master>
