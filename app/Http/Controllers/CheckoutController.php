c57aa06803c89e25b02ea3468a576a4110c2ad5a
HopeWu <767088240@qq.com>
2021-12-08 16:54:26 +0800

    revert last

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -11,14 +11,10 @@ class CheckoutController extends Controller
         $start_end = $request->session()->get('start_end');
         $html_snippet = $request->session()->get('html_snippet');
 
-        if(isset($appointment)){
-            return view('to-pay', [
-                'appt' => $appointment,
-                'start_end' => $start_end,
-                'html_snippet' => $html_snippet
-            ]);
-        }else{
-            redirect(route('fill-form'));
-        }
+        return view('to-pay', [
+            'appt' => $appointment,
+            'start_end' => $start_end,
+            'html_snippet' => $html_snippet
+        ]);
     }
 }
