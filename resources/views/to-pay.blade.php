fe29099b1f98a8c28855000d995077607e086157
HopeWu <767088240@qq.com>
2021-12-08 22:07:39 +0800

    leave only the klarna snippet in page to-pay.blade.php

--- resources/views/to-pay.blade.php
+++ resources/views/to-pay.blade.php
@@ -1,17 +1,3 @@
 <x-master>
-    <article class="confirm">
-        <h6>
-            <!--
-            Please check the appointment before you continue to make the payment
-            -->
-            Vänligen kontrollera mötet innan du fortsätter att göra betalningen.
-        </h6>
-        <div>
-            <p>
-                Den {{$appt->date}}, från kl {{$start_end[0]}} till kl {{$start_end[1]}}, via ZOOM.
-            </p>
-        </div>
-    </article>
-
     {!! $html_snippet !!}
 </x-master>
