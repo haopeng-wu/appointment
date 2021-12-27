cb2058e7ec0826ebca90e412b9b7fa9c42ff67cd
HopeWu <767088240@qq.com>
2021-12-27 21:08:01 +0800

    Fix rel-123. Gracefully handle csrf expiration exception.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +38,20 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof TokenMismatchException)
+        {
+            return redirect()
+                ->back()
+                ->withInput($request->except('password'))
+                ->with([
+                    'message' => 'Page expired. Please try again.',
+                    'message-type' => 'TokenMismatchException'
+                ]);
+        }
+        return parent::render($request, $e);
     }
 }
