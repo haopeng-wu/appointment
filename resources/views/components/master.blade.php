af0f21cde414a5828df496e013b7f2c8de7baf06
HopeWu <767088240@qq.com>
2021-12-28 18:39:56 +0800

    change the path of the favicon code in the head of master.blade.php

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -11,11 +11,11 @@
     -->
     <link rel="stylesheet" type="text/css" href="https://npmcdn.com/flatpickr/dist/themes/airbnb.css">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('images/favicon_package_v0/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('images/favicon_package_v0/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('images/favicon_package_v0/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('images/favicon_package_v0/site.webmanifest')}}">
+    <link rel="mask-icon" href="{{asset('images/favicon_package_v0/safari-pinned-tab.svg')}}" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#00aba9">
     <meta name="theme-color" content="#ffffff">
 
