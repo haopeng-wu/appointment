711ba17b9ca506d1602d5497e92e617a0023acf2
HopeWu <767088240@qq.com>
2021-12-28 17:42:51 +0800

    change favicon file

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     -->
     <link rel="stylesheet" type="text/css" href="https://npmcdn.com/flatpickr/dist/themes/airbnb.css">
-    <link rel="icon" href="{{asset('images/icon.svg')}}">
+    <link rel="icon" type="image/png" href="{{asset('images/favicon.png')}}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
