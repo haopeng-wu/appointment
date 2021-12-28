1549130039e0f3013fe72d7b1d573e4578652d71
HopeWu <767088240@qq.com>
2021-12-28 18:36:33 +0800

    Insert into the header the favicon code. Need Changing Path!!

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -10,7 +10,15 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     -->
     <link rel="stylesheet" type="text/css" href="https://npmcdn.com/flatpickr/dist/themes/airbnb.css">
-    <link rel="shortcut icon" type="apple-touch-icon" href="{{asset('images/favicon.ico')}}">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#00aba9">
+    <meta name="theme-color" content="#ffffff">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
