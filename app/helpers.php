ec8cc7d27d4e430ce280ebda7bca3e0696358185
HopeWu <767088240@qq.com>
2022-01-06 17:02:21 +0800

    clear useless helper function

--- app/helpers.php
+++ app/helpers.php
@@ -8,35 +8,3 @@
 
 use App\Models\User;
 use Illuminate\Support\Facades\Cookie;
-/*
-function loginOrCreate(){
-    $u_id_cookie = request()->cookie('user_id');
-    if ($u_id_cookie and $user=User::find($u_id_cookie)){
-        session(['current_user'=>$user->id]);
-        return $user;
-    }else{
-        # first create this user
-        $user=User::create();
-
-        # set the user_id cookies
-        $minutes=30*24*60;  # remember a user for a month
-        Cookie::queue('user_id', $user->id, $minutes);
-        session(['current_user'=>$user->id]);
-        return $user;
-    }
-}
-*/
-function loginOrCreate(){
-    $u_id_session = session('user_id');
-    if ($u_id_session and $user=User::find($u_id_session))
-    {
-        return $user;
-    }else{
-        # first create this user
-        $user=User::create();
-
-        # set the user_id session
-        session(['user_id'=>$user->id]);
-        return $user;
-    }
-}
