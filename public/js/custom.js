ea8f37db1a28eba50b9782d07d5f49999a805c78
HopeWu <767088240@qq.com>
2022-01-23 18:30:39 +0800

    rel-113, develop menu bar font end developing fix js file bug

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,19 +3,22 @@ for slots select color
 */
 let slots = document.querySelector(".time-slots");
 
-slots.addEventListener("change", function (event) {
-    //console.log(event.target.id);
+if (slots != null){
+    slots.addEventListener("change", function (event) {
+        //console.log(event.target.id);
 
-    let slotLabels = document.querySelectorAll(".time-slots label");
-    for(let i = 0; i < slotLabels.length; i++) {
-        slotLabels[i].classList.remove("checked");
-        //slotLabels[i].classList.add("unchecked");
-    }
+        let slotLabels = document.querySelectorAll(".time-slots label");
+        for(let i = 0; i < slotLabels.length; i++) {
+            slotLabels[i].classList.remove("checked");
+            //slotLabels[i].classList.add("unchecked");
+        }
 
-    let theLable = document.querySelector(`label[for=${event.target.id}]`);
-    //theLable.classList.remove("unchecked");
-    theLable.classList.add("checked");
-})
+        let theLable = document.querySelector(`label[for=${event.target.id}]`);
+        //theLable.classList.remove("unchecked");
+        theLable.classList.add("checked");
+    })
+}
+
 
 flatpickr('.date input',{
     altInput: true,
@@ -54,77 +57,84 @@ function clearClasses(element){
 
 const dateInput = document.querySelector('.date input#date');
 
-dateInput.addEventListener('input', function (){
-    /*
-    Do the cleaning first:
-        a) Remove all the classes of all the labels
-        b) Enable all the input radio buttons by setting disabled = false
-        c) Deselect all the input radios buttons
-     */
-    let slotLabels = document.querySelectorAll(".time-slots label");
-    // Remove all the classes of all the labels
-    for(let i = 0; i < slotLabels.length; i++) {
-        clearClasses(slotLabels[i]);
-    }
-    let slotInputs = document.querySelectorAll(".time-slots input");
-    for(let i = 0; i < slotInputs.length; i++) {
-        // Enable all the input radio buttons by setting disabled = false
-        slotInputs[i].disabled = false;
-        // Deselect all the input radios buttons
-        slotInputs[i].checked = false;
-    }
-
-    //console.log(allFutureBooked);
-    let bookedSlots = allFutureBooked[dateInput.value];
-    if(bookedSlots){
-        for(let i = 0; i < bookedSlots.length; i++){
-            const theLabel = document.querySelector(`label[for=slot-${bookedSlots[i]}]`);
-            const theInputElement = document.querySelector(`input#slot-${bookedSlots[i]}`);
-            console.log('theInputElement'+theInputElement);
-            clearClasses(theLabel);
-            theLabel.classList.add('booked');
-            theInputElement.disabled = true;
+if (dateInput != null)
+{
+    dateInput.addEventListener('input', function (){
+        /*
+        Do the cleaning first:
+            a) Remove all the classes of all the labels
+            b) Enable all the input radio buttons by setting disabled = false
+            c) Deselect all the input radios buttons
+         */
+        let slotLabels = document.querySelectorAll(".time-slots label");
+        // Remove all the classes of all the labels
+        for(let i = 0; i < slotLabels.length; i++) {
+            clearClasses(slotLabels[i]);
         }
-    }
+        let slotInputs = document.querySelectorAll(".time-slots input");
+        for(let i = 0; i < slotInputs.length; i++) {
+            // Enable all the input radio buttons by setting disabled = false
+            slotInputs[i].disabled = false;
+            // Deselect all the input radios buttons
+            slotInputs[i].checked = false;
+        }
 
-    /*
-    disable all the passed time slots in today
-     */
-    //console.log(dateInput.value);
-    const selectedDate = new Date(dateInput.value);
-    let todayDate = new Date();
-    if (selectedDate.setHours(0, 0, 0, 0) === todayDate.setHours(0, 0, 0, 0)){
-        //console.log(all_slots);
-        // now disable the slots that has passed away
-        console.log('Today');
-        //console.log(all_slots);
-        if (all_slots){
-            let h = '', m = '', slotTime = '';
-            let todayDate = new Date();
-            let now = new Date(0, 0, 0, todayDate.getHours(), todayDate.getMinutes());
-            console.log(now);
-            for (const key in all_slots){
-                console.log(key);
-                h = all_slots[key].split(' - ')[0].split(':')[0];
-                m = all_slots[key].split(' - ')[0].split(':')[1];
-                slotTime = new Date(0, 0, 0, h, m);
-                console.log(now - slotTime);
-                if (now - slotTime > 0){
-                    const _theLabel = document.querySelector(`label[for=slot-${key}]`);
-                    const _theInputElement = document.querySelector(`input#slot-${key}`);
-                    clearClasses(_theLabel);
-                    _theLabel.classList.add('booked');
-                    _theInputElement.disabled = true;
-                }
+        //console.log(allFutureBooked);
+        let bookedSlots = allFutureBooked[dateInput.value];
+        if(bookedSlots){
+            for(let i = 0; i < bookedSlots.length; i++){
+                const theLabel = document.querySelector(`label[for=slot-${bookedSlots[i]}]`);
+                const theInputElement = document.querySelector(`input#slot-${bookedSlots[i]}`);
+                console.log('theInputElement'+theInputElement);
+                clearClasses(theLabel);
+                theLabel.classList.add('booked');
+                theInputElement.disabled = true;
             }
         }
-    }else {
-        console.log('not today');
-    }
-})
+
+        /*
+        disable all the passed time slots in today
+         */
+        //console.log(dateInput.value);
+        const selectedDate = new Date(dateInput.value);
+        let todayDate = new Date();
+        if (selectedDate.setHours(0, 0, 0, 0) === todayDate.setHours(0, 0, 0, 0)){
+            //console.log(all_slots);
+            // now disable the slots that has passed away
+            console.log('Today');
+            //console.log(all_slots);
+            if (all_slots){
+                let h = '', m = '', slotTime = '';
+                let todayDate = new Date();
+                let now = new Date(0, 0, 0, todayDate.getHours(), todayDate.getMinutes());
+                console.log(now);
+                for (const key in all_slots){
+                    console.log(key);
+                    h = all_slots[key].split(' - ')[0].split(':')[0];
+                    m = all_slots[key].split(' - ')[0].split(':')[1];
+                    slotTime = new Date(0, 0, 0, h, m);
+                    console.log(now - slotTime);
+                    if (now - slotTime > 0){
+                        const _theLabel = document.querySelector(`label[for=slot-${key}]`);
+                        const _theInputElement = document.querySelector(`input#slot-${key}`);
+                        clearClasses(_theLabel);
+                        _theLabel.classList.add('booked');
+                        _theInputElement.disabled = true;
+                    }
+                }
+            }
+        }else {
+            console.log('not today');
+        }
+    })
+}
+
 
 window.setTimeout(() => {
-    document.querySelector('div.message').classList.add('hide')
+    let message = document.querySelector('div.message');
+    if (message != null){
+        message.classList.add('hide');
+    }
 }, 5000)
 
 
