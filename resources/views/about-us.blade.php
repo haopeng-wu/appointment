0d5762eb59558ba79eae5698927c7a95b65822d3
Markusrelutv <97878312+Markusrelutv@users.noreply.github.com>
2022-01-28 09:12:12 +0100

    Update about us page
    Added about us information in Swedish

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,5 +1,43 @@
 <x-master>
     <h1>
         About Us
+
+Vad erbjuder vi?
+Familjerådgivning och parterapi
+•	Svårt att prata med varandra
+•	Upptrappade konflikter eller långvarig tystnad mellan er
+•	Familjebildningproblematik
+•	Känslorna har förändrats
+•	Någon av er har träffat en annan
+•	Otrohet
+•	Tankar på separation
+•	Sexuella svårigheter
+•	Svårt få ihop sin bonusfamilj
+•	Svårigheter i relationen mellan vuxna syskon, till en förälder, svärförälder
+Vi är redo
+Krisbearbetning
+•	Ålders/utveckligskris
+•	Depression
+•	Allvarlig fysisk sjukdom
+•	Separation
+•	Anhörigs död
+Komma igång
+Samtalsterapi/ KBT/ fobiträning
+•	Bearbetning av känslor och tankemönster som leder till oönskade
+•	reaktioner, handlingar, undvikanden.
+•	Aggressionsproblematik
+•	Olika former av fobier
+Vad har vi för bakgrund?
+Utbildning
+•	Socionom
+•	Kognitiv psykoterapi
+•	Sexologi
+Lång arbetslivserfarenhet som
+•	Behandlare beroendeproblematik
+•	Socionom slutenvårdspsykiatri
+•	Kurator på vårdcentral
+•	Auktoriserad familjerådgivare
+
+
     </h1>
 </x-master>
