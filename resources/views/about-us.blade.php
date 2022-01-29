40faee14fa59b007b66990f325efdf40eed4bb2b
Markusrelutv <97878312+Markusrelutv@users.noreply.github.com>
2022-01-29 16:15:01 +0100

    Update about-us.blade.php
    Updated text formatting

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,43 +1,62 @@
 <x-master>
-    <h1>
-        About Us
+    
+        <h1>Om oss</h1>
 
-Vad erbjuder vi?
-Familjerådgivning och parterapi
-•	Svårt att prata med varandra
-•	Upptrappade konflikter eller långvarig tystnad mellan er
-•	Familjebildningproblematik
-•	Känslorna har förändrats
-•	Någon av er har träffat en annan
-•	Otrohet
-•	Tankar på separation
-•	Sexuella svårigheter
-•	Svårt få ihop sin bonusfamilj
-•	Svårigheter i relationen mellan vuxna syskon, till en förälder, svärförälder
-Vi är redo
-Krisbearbetning
-•	Ålders/utveckligskris
-•	Depression
-•	Allvarlig fysisk sjukdom
-•	Separation
-•	Anhörigs död
-Komma igång
-Samtalsterapi/ KBT/ fobiträning
-•	Bearbetning av känslor och tankemönster som leder till oönskade
-•	reaktioner, handlingar, undvikanden.
-•	Aggressionsproblematik
-•	Olika former av fobier
-Vad har vi för bakgrund?
-Utbildning
-•	Socionom
-•	Kognitiv psykoterapi
-•	Sexologi
-Lång arbetslivserfarenhet som
-•	Behandlare beroendeproblematik
-•	Socionom slutenvårdspsykiatri
-•	Kurator på vårdcentral
-•	Auktoriserad familjerådgivare
+    <p><strong><span style="font-size:20px">Vad kan vi erbjuda er/dig?</span></strong></p>
 
+<p><strong><span style="font-size:18px">Familjer&aring;dgivning och parterapi</span></strong></p>
 
-    </h1>
+<ul>
+	<li><span style="font-size:16px">Sv&aring;rt att prata med varandra</span></li>
+	<li><span style="font-size:16px">Upptrappade konflikter eller l&aring;ngvarig tystnad mellan er</span></li>
+	<li><span style="font-size:16px">Familjebildningproblematik</span></li>
+	<li><span style="font-size:16px">K&auml;nslorna har f&ouml;r&auml;ndrats</span></li>
+	<li><span style="font-size:16px">N&aring;gon av er har tr&auml;ffat en annan</span></li>
+	<li><span style="font-size:16px">Otrohet</span></li>
+	<li><span style="font-size:16px">Tankar p&aring; separation</span></li>
+	<li><span style="font-size:16px">Sexuella sv&aring;righeter</span></li>
+	<li><span style="font-size:16px">Sv&aring;rt f&aring; ihop sin bonusfamilj</span></li>
+	<li><span style="font-size:16px">Sv&aring;righeter i relationen mellan vuxna syskon, till en f&ouml;r&auml;lder, sv&auml;rf&ouml;r&auml;lder</span></li>
+</ul>
+
+<p><strong><span style="font-size:18px">Krisbearbetning</span></strong></p>
+
+<ul>
+	<li><span style="font-size:16px">&Aring;lders/utveckligskris</span></li>
+	<li><span style="font-size:16px">Depression</span></li>
+	<li><span style="font-size:16px">Allvarlig fysisk sjukdom</span></li>
+	<li><span style="font-size:16px">Separation</span></li>
+	<li><span style="font-size:16px">Anh&ouml;rigs d&ouml;d</span></li>
+</ul>
+
+<p><strong><span style="font-size:18px">Samtalsterapi / KBT / fobitr&auml;ning</span></strong></p>
+
+<ul>
+	<li><span style="font-size:16px">Bearbetning av k&auml;nslor och tankem&ouml;nster som leder till o&ouml;nskade reaktioner, handlingar, undvikanden.</span></li>
+	<li><span style="font-size:16px">Aggressionsproblematik</span></li>
+	<li><span style="font-size:16px">Olika former av fobier</span></li>
+</ul>
+
+<p><strong><span style="font-size:20px">Vad g&ouml;r oss kvalificerade att hj&auml;lpa er/dig?</span></strong></p>
+
+<p><strong><span style="font-size:18px">Utbildning</span></strong></p>
+
+<ul>
+	<li><span style="font-size:16px">Socionom</span></li>
+	<li><span style="font-size:16px">Kognitiv psykoterapi</span></li>
+	<li><span style="font-size:16px">Sexologi</span></li>
+</ul>
+
+<p><strong><span style="font-size:18px">L&aring;ng arbetslivserfarenhet som</span></strong></p>
+
+<ul>
+	<li><span style="font-size:16px">Behandlare beroendeproblematik</span></li>
+	<li><span style="font-size:16px">Socionom slutenv&aring;rdspsykiatri</span></li>
+	<li><span style="font-size:16px">Kurator p&aring; v&aring;rdcentral</span></li>
+	<li><span style="font-size:16px">Auktoriserad familjer&aring;dgivare</span></li>
+</ul>
+    
+    
+    
+    
 </x-master>
