b2fc7164ca4d0354e4c0d213cde3c5356cd2bcc6
root <root@vps.server.local>
2022-01-31 05:06:38 +0000

    scroll to fade in

--- public/js/custom.js
+++ public/js/custom.js
@@ -169,7 +169,7 @@ function isVisible(element){
     let elementBox = element.getBoundingClientRect();
     let distanceFromTop = -200;
 
-    if (element.top - window.innerHeight < distanceFromTop){
+    if (elementBox.top - window.innerHeight < distanceFromTop){
         return true;
     }else{
         return false;
