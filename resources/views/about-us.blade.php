6f67f0650f71c7694a74c731c3cafb096eab0502
HopeWu <767088240@qq.com>
2022-02-06 09:57:10 +0800

    format the about us page

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,62 +1,75 @@
 <x-master>
-    
-        <h1>Om oss</h1>
 
-    <p><strong><span style="font-size:20px">Vad kan vi erbjuda er/dig?</span></strong></p>
+    <main>
+        <article>
+            <h1>Om oss</h1>
 
-<p><strong><span style="font-size:18px">Familjer&aring;dgivning och parterapi</span></strong></p>
+            <p><strong><span style="font-size:20px">Vad kan vi erbjuda er/dig?</span></strong></p>
 
-<ul>
-	<li><span style="font-size:16px">Sv&aring;rt att prata med varandra</span></li>
-	<li><span style="font-size:16px">Upptrappade konflikter eller l&aring;ngvarig tystnad mellan er</span></li>
-	<li><span style="font-size:16px">Familjebildningproblematik</span></li>
-	<li><span style="font-size:16px">K&auml;nslorna har f&ouml;r&auml;ndrats</span></li>
-	<li><span style="font-size:16px">N&aring;gon av er har tr&auml;ffat en annan</span></li>
-	<li><span style="font-size:16px">Otrohet</span></li>
-	<li><span style="font-size:16px">Tankar p&aring; separation</span></li>
-	<li><span style="font-size:16px">Sexuella sv&aring;righeter</span></li>
-	<li><span style="font-size:16px">Sv&aring;rt f&aring; ihop sin bonusfamilj</span></li>
-	<li><span style="font-size:16px">Sv&aring;righeter i relationen mellan vuxna syskon, till en f&ouml;r&auml;lder, sv&auml;rf&ouml;r&auml;lder</span></li>
-</ul>
+            <p><strong><span style="font-size:18px">Familjer&aring;dgivning och parterapi</span></strong></p>
 
-<p><strong><span style="font-size:18px">Krisbearbetning</span></strong></p>
+            <ul>
+                <li><span style="font-size:16px">Sv&aring;rt att prata med varandra</span></li>
+                <li><span style="font-size:16px">Upptrappade konflikter eller l&aring;ngvarig tystnad mellan er</span>
+                </li>
+                <li><span style="font-size:16px">Familjebildningproblematik</span></li>
+                <li><span style="font-size:16px">K&auml;nslorna har f&ouml;r&auml;ndrats</span></li>
+                <li><span style="font-size:16px">N&aring;gon av er har tr&auml;ffat en annan</span></li>
+                <li><span style="font-size:16px">Otrohet</span></li>
+                <li><span style="font-size:16px">Tankar p&aring; separation</span></li>
+                <li><span style="font-size:16px">Sexuella sv&aring;righeter</span></li>
+                <li><span style="font-size:16px">Sv&aring;rt f&aring; ihop sin bonusfamilj</span></li>
+                <li><span style="font-size:16px">Sv&aring;righeter i relationen mellan vuxna syskon, till en f&ouml;r&auml;lder, sv&auml;rf&ouml;r&auml;lder</span>
+                </li>
+            </ul>
+        </article>
 
-<ul>
-	<li><span style="font-size:16px">&Aring;lders/utveckligskris</span></li>
-	<li><span style="font-size:16px">Depression</span></li>
-	<li><span style="font-size:16px">Allvarlig fysisk sjukdom</span></li>
-	<li><span style="font-size:16px">Separation</span></li>
-	<li><span style="font-size:16px">Anh&ouml;rigs d&ouml;d</span></li>
-</ul>
+        <article>
+            <p><strong><span style="font-size:18px">Krisbearbetning</span></strong></p>
 
-<p><strong><span style="font-size:18px">Samtalsterapi / KBT / fobitr&auml;ning</span></strong></p>
+            <ul>
+                <li><span style="font-size:16px">&Aring;lders/utveckligskris</span></li>
+                <li><span style="font-size:16px">Depression</span></li>
+                <li><span style="font-size:16px">Allvarlig fysisk sjukdom</span></li>
+                <li><span style="font-size:16px">Separation</span></li>
+                <li><span style="font-size:16px">Anh&ouml;rigs d&ouml;d</span></li>
+            </ul>
+        </article>
 
-<ul>
-	<li><span style="font-size:16px">Bearbetning av k&auml;nslor och tankem&ouml;nster som leder till o&ouml;nskade reaktioner, handlingar, undvikanden.</span></li>
-	<li><span style="font-size:16px">Aggressionsproblematik</span></li>
-	<li><span style="font-size:16px">Olika former av fobier</span></li>
-</ul>
+        <article>
+            <p><strong><span style="font-size:18px">Samtalsterapi / KBT / fobitr&auml;ning</span></strong></p>
 
-<p><strong><span style="font-size:20px">Vad g&ouml;r oss kvalificerade att hj&auml;lpa er/dig?</span></strong></p>
+            <ul>
+                <li><span style="font-size:16px">Bearbetning av k&auml;nslor och tankem&ouml;nster som leder till o&ouml;nskade reaktioner, handlingar, undvikanden.</span>
+                </li>
+                <li><span style="font-size:16px">Aggressionsproblematik</span></li>
+                <li><span style="font-size:16px">Olika former av fobier</span></li>
+            </ul>
+        </article>
 
-<p><strong><span style="font-size:18px">Utbildning</span></strong></p>
+        <article>
+            <p><strong><span
+                            style="font-size:20px">Vad g&ouml;r oss kvalificerade att hj&auml;lpa er/dig?</span></strong>
+            </p>
 
-<ul>
-	<li><span style="font-size:16px">Socionom</span></li>
-	<li><span style="font-size:16px">Kognitiv psykoterapi</span></li>
-	<li><span style="font-size:16px">Sexologi</span></li>
-</ul>
+            <p><strong><span style="font-size:18px">Utbildning</span></strong></p>
 
-<p><strong><span style="font-size:18px">L&aring;ng arbetslivserfarenhet som</span></strong></p>
+            <ul>
+                <li><span style="font-size:16px">Socionom</span></li>
+                <li><span style="font-size:16px">Kognitiv psykoterapi</span></li>
+                <li><span style="font-size:16px">Sexologi</span></li>
+            </ul>
+        </article>
 
-<ul>
-	<li><span style="font-size:16px">Behandlare beroendeproblematik</span></li>
-	<li><span style="font-size:16px">Socionom slutenv&aring;rdspsykiatri</span></li>
-	<li><span style="font-size:16px">Kurator p&aring; v&aring;rdcentral</span></li>
-	<li><span style="font-size:16px">Auktoriserad familjer&aring;dgivare</span></li>
-</ul>
-    
-    
-    
-    
+        <article>
+            <p><strong><span style="font-size:18px">L&aring;ng arbetslivserfarenhet som</span></strong></p>
+
+            <ul>
+                <li><span style="font-size:16px">Behandlare beroendeproblematik</span></li>
+                <li><span style="font-size:16px">Socionom slutenv&aring;rdspsykiatri</span></li>
+                <li><span style="font-size:16px">Kurator p&aring; v&aring;rdcentral</span></li>
+                <li><span style="font-size:16px">Auktoriserad familjer&aring;dgivare</span></li>
+            </ul>
+        </article>
+    </main>
 </x-master>
