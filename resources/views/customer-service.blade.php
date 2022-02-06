59360ac6df9f8a83586e4a0d212148ac8948c1e6
HopeWu <767088240@qq.com>
2022-02-06 10:07:31 +0800

    alter some css in customer-service.blade.php

--- resources/views/customer-service.blade.php
+++ resources/views/customer-service.blade.php
@@ -1,5 +1,8 @@
 <x-master>
-    <h1>Kundservice</h1>
-    För frågor om tidsbolkning och om existerande bokning etc. kan ni nå oss måndag-fredag helgfria vardagar mellan kl 17-20 på 07X-XXX XX XX.
-    
+    <main>
+        <article>
+            <h1>Kundservice</h1>
+            För frågor om tidsbolkning och om existerande bokning etc. kan ni nå oss måndag-fredag helgfria vardagar mellan kl 17-20 på 07X-XXX XX XX.
+        </article>
+    </main>
 </x-master>
